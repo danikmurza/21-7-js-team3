8a0a4cec8f9ae66ac3f7741f86306ffc0feec343
Pirci <emrepirci@gmail.com>
2021-09-02 00:21:52 +0200

    Age calculator homework

---
+++ Pirci/js-intro/age-calculator.js
@@ -0,0 +1,17 @@
+/* The Age Calculator
+Forgot how old someone is? Calculate it!
+
+Store the current year in a variable.
+Store their birth year in a variable.
+Calculate their 2 possible ages based on the stored values.
+Output them to the screen like so: "They are either NN or NN", substituting the values. */
+
+let currentYear = 2021;
+const birth = 1989;
+
+let ageOne = currentYear - birth;
+let ageTwo = ageOne - 1;
+
+let howOld = `You are either ${ageOne} or ${ageTwo} years old. It depends on what month you were born.`;
+
+console.log(howOld);
