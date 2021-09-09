529781491447f48ac0ef2e17e692f9e627739c39
Your Name <you@example.com>
2021-09-09 16:25:23 -0500

    SimpleTimer repair

--- danikmurza/Data Structure Practice/a-simple-timer-ds-js/index.js
+++ danikmurza/Data Structure Practice/a-simple-timer-ds-js/index.js
@@ -3,17 +3,21 @@ let simpleTimer = (num) => {
     let minute = Math.floor(num / 60);
     let minutes = minute
     let sec = num % 60;
-    if (sec + ''.length < 2) {
-        sec = '0' + sec;
-    }
     if (minute > 60) {
-      let a =  (minute / 60).toFixed()
+        let a = (minute / 60).toFixed()
         minutes = minute - (a * 60)
     }
-
-
+    if (sec + ''.length < 10) {
+        sec = '0' + sec;
+    }
+    if (hour < 10) {
+        hour = '0' + hour
+    }
+    if (minutes < 10) {
+        minutes = '0' + minutes
+    }
     return hour + ":" + minutes + ":" + sec;
 }
 
-console.log(simpleTimer(22222))
+console.log(simpleTimer(7777))
 
